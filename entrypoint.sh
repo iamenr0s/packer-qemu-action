0d22028dd13ebdb4774ade21178153240d1d7690
enr0s <info@enros.me>
2020-08-25 00:14:54 +0100

    fixed shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 cd "${INPUT_WORKINGDIR:-.}"
 
@@ -7,8 +8,10 @@ if [[ ! -f "$INPUT_TEMPLATEFILE" ]]; then
     exit 1
 fi
 
+set +e
 BUILD_OUTPUT=$(sh -c "PACKER_LOG=1 packer build ${INPUT_TEMPLATEFILE}" 2>&1)
 BUILD_SUCCESS=$?
 echo "$BUILD_OUTPUT"
+set -e
 
 exit $BUILD_SUCCESS
